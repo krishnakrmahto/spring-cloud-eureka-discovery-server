6cc9e939c0110bfcb3fa244ef9f8f5626a7f586c
krishnakrmahto97 <krish.thorcode@gmail.com>
2022-01-29 22:23:24 +0530

    standalone eureka discovery server setup

---
+++ src/test/java/com/sampleprojects/ecommercestore/eurekadiscoveryserver/EurekaServiceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampleprojects.ecommercestore.eurekadiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
